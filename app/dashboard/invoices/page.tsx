f539eb5a73bffdceed5749515d5e666d1a30d5c4
na-lovable <logintotrystuff@gmail.com>
2025-08-16 09:48:14 +0530

    Upto chapter 9, Suspense

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
+export default async function Page() {
+    // Artificially delay a response for demo purposes.
+        // Don't do this in production :)
+    
+        console.log('Fetching invoices data...');
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+    
+        console.log('Page load completed after 5 seconds.');
     return <p>Invoices Page</p>
 }
