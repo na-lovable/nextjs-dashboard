86b7887930a3929bb36056c670c62cce19e0ab9c
na-lovable <logintotrystuff@gmail.com>
2025-08-16 13:33:26 +0530

    upto chapter 11, Search

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,9 +2,9 @@ export default async function Page() {
     // Artificially delay a response for demo purposes.
         // Don't do this in production :)
     
-        console.log('Fetching customers data...');
-        await new Promise((resolve) => setTimeout(resolve, 5000));
+        // console.log('Fetching customers data...');
+        // await new Promise((resolve) => setTimeout(resolve, 5000));
     
-        console.log('Page load completed after 5 seconds.');
+        // console.log('Page load completed after 5 seconds.');
     return <p>Customers Page</p>
 }
